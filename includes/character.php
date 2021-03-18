58270da20ab1bb271b78b1ebe64a4b65bd22218d
Bas Verdoorn <99061684@mydavinci.nl>
2021-03-18 11:17:44 +0100

    commit 5
    wat styling aangepast en de code wat netter gemaakt.

--- includes/character.php
+++ includes/character.php
@@ -5,7 +5,6 @@
     $stmt = $conn->prepare("SELECT * FROM characters");
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    // print_r($result); //print resultaat
     
     $id = ($_GET["id"] - 1);
     $value = $result[$id];
@@ -20,36 +19,38 @@
     <meta charset="UTF-8">
     <title>Character - Bowser</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link href="resources/css/style.css" rel="stylesheet"/>
+    <link href="../resources/css/style.css" rel="stylesheet"/>
 </head>
 <body>
 <header>
     <h1><?php echo $value["name"];?></h1>
     <a class="backbutton" href="../index.php"><i class="fas fa-long-arrow-alt-left"></i> Terug</a>
 </header>
-<div id="container">
-    <div class="detail">
-        <div class="left">
-            <img class="avatar" src='../resources/images/<?php echo $value["avatar"];?>'>
-            
-            <div class="stats" style="background-color: <?php echo $value["color"];?>">
-                <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fas fa-heart"></i></span> <?php echo $value["health"];?></li>
-                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> <?php echo $value["attack"];?></li>
-                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> <?php echo $value["defense"];?></li>
-                </ul>
-                <ul class="gear">
-                    <li><b>Weapon</b>: <?php echo $value["weapon"];?></li>
-                    <li><b>Armor</b>: <?php echo $value["armor"];?></li>
-                </ul>
+<div class="body_container">
+    <div class="container">
+        <div class="detail">
+            <div class="left">
+                <img class="avatar" src='../resources/images/<?php echo $value["avatar"];?>' <?php if($value["id"] == 2){echo "style='transform: rotate(270deg)'";}?>>
+                
+                <div class="stats" style="background-color: <?php echo $value["color"];?>">
+                    <ul class="fa-ul">
+                        <li><span class="fa-li"><i class="fas fa-heart"></i></span> <?php echo $value["health"];?></li>
+                        <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> <?php echo $value["attack"];?></li>
+                        <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> <?php echo $value["defense"];?></li>
+                    </ul>
+                    <ul class="gear">
+                        <?php if($value["weapon"] != NULL){echo "<li><b>Weapon</b>: ".$value["weapon"]."</li>";}?>
+                        <?php if($value["armor"] != NULL){echo "<li><b>Armor</b>: ".$value["armor"]."</li>";}?>
+                    </ul>
+                </div>
             </div>
+            <div class="right">
+                <p>
+                    <?php echo nl2br($value["bio"], false);?>
+                </p>
+            </div>
+            <div style="clear: both"></div>
         </div>
-        <div class="right">
-            <p>
-                <?php echo $value["bio"];?>
-            </p>
-        </div>
-        <div style="clear: both"></div>
     </div>
 </div>
 <footer>&copy; [Bas Verdoorn] 2021</footer>
--- index.php
+++ index.php
@@ -5,13 +5,10 @@
     $stmt = $conn->prepare("SELECT count(*) AS aantal FROM characters");
     $stmt->execute(); 	
     $result1 = $stmt->fetch(PDO::FETCH_ASSOC);
-    // print_r($result1); //print resultaat
-    // echo $result1["aantal"];
 
     $stmt = $conn->prepare("SELECT * FROM characters ORDER BY name");
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    // print_r($result); //print resultaat
 
     //sluit connectie
     $conn = null;
@@ -30,46 +27,30 @@
     <h1>Alle <?php echo $result1["aantal"]; ?> characters uit de database</h1>
 </header>
 
+    <div class="body_container">
 <?php
     foreach ($result as $array => $value) {
-        echo "<div id='container'>";
-            echo "<a class='item' href=".'includes/character.php?id='.$value["id"].">";
-                echo "<div id='left'>";
-                    echo "<img class='avatar' src=".'resources/images/'.$value["avatar"].">";
-                echo "</div>";
-                echo "<div id='right'>";
-                    echo "<h2>".$value["name"]."</h2>";
-                    echo "<div id='stats'>";
-                        echo "<ul class='fa-ul'>
+        echo "<div class='container'>
+            <a class='item' href=".'includes/character.php?id='.$value["id"].">
+                <div class='left'>
+                    <img class='avatar' src=".'resources/images/'.$value["avatar"]; if($value["id"] == 2){echo " style='transform: rotate(270deg)'";} echo ">
+                    </div>
+                    <div class='right'>
+                    <h2>".$value["name"]."</h2>
+                        <div class='stats'>
+                            <ul class='fa-ul'>
                             <li><span class='fa-li'><i class='fas fa-heart'></i></span>".$value["health"]."</li>
                             <li><span class='fa-li'><i class='fas fa-fist-raised'></i></span>".$value["attack"]."</li>
                             <li><span class='fa-li'><i class='fas fa-shield-alt'></i></span>".$value["defense"]."</li>
-                        </ul>";
-                    echo "</div>";
-                echo "</div>";
-                echo "<div id='detailButton'><i class='fas fa-search'></i> bekijk</div>";
-            echo "</a>";
-        echo "</div>";
+                        </ul>
+                        </div>
+                    </div>
+                    <div class='detailButton'><i class='fas fa-search'></i> bekijk</div>
+                </a>
+            </div>";
     }
 ?>
-<!-- <div id="container">
-    <a class="item" href="character.html">
-        <div class="left">
-            <img class="avatar" src="resources/images/bowser.jpg">
-        </div>
-        <div class="right">
-            <h2>Bowser</h2>
-            <div class="stats">
-                <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fas fa-heart"></i></span> 10000</li>
-                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> 400</li>
-                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> 100</li>
-                </ul>
-            </div>
-        </div>
-        <div class="detailButton"><i class="fas fa-search"></i> bekijk</div>
-    </a>
-</div> -->
+    </div>
 <footer>&copy; [Bas Verdoorn] 2021</footer>
 </body>
 </html>
